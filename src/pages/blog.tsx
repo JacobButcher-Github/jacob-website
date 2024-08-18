6911bcbf2d427ff8c61219e845e850ed60af6fd2
Jacob <osarukun@live.com>
2024-08-17 21:27:17 -0400

    WIP: Style the blog list page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -15,14 +15,15 @@ export default function Blog({ allPostsData }: { allPostsData: PostData[] }) {
   return (
     <>
       <Head>
-        <title>Jacob Butcher</title>
+        <title>About - Jacob Butcher</title>
       </Head>
       <div className="bg-gradient-to-b from-bgMain to-bgSecondary font-motiva-sans min-h-screen">
         <NavBar />
 
         <div className="flex flex-col items-center text-mainText">
-          <h2 className="">Blog</h2>
-          <ul className="">
+          <h1 className="text-7xl mt-16">Blog</h1>
+          <div className="w-1/3 h-[2px] bg-gradient-to-r from-transparent via-mainText to-transparent mb-8"></div>
+          <ul className="text-2xl">
             {allPostsData.map(({ id, date, title }) => (
               <li className="" key={id}>
                 {title}
