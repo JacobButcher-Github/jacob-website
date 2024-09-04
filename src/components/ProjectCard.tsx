0ec4c127dbf2feda48f6feff2b2c2577867cae54
Jacob <osarukun@live.com>
2024-09-04 15:49:08 -0400

    WIP: ProjectCard first version w/ PreviewPane included

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,24 +4,30 @@ import { ProjectItem } from "./ProjectList";
 import PreviewPane from "./PreviewPane";
 
 interface Props {
-  item: ProjectItem;
+  projectItems: ProjectItem[];
 }
 
 const ProjectCard = (props: Props) => {
-  const { item } = props;
+  const { projectItems } = props;
 
   return (
     <>
-      <Link href={item.route}>
-        <div className="bg-bgMain hover:bg-bgHover flex flex-row items-start w-full">
-          <PreviewPane item={item} />
-
-          <div className="w-2/3 flex flex-col text-mainText">
-            <div className="text-2xl">{item.title}</div>
-            <div className="">{item.description}</div>
-          </div>
-        </div>
-      </Link>
+      <div>
+        {projectItems.map((item) => (
+          <Link
+            key={item.route}
+            href={item.route}
+          >
+            <div className="bg-bgMain hover:bg-bgHover flex flex-row items-start w-full">
+              <PreviewPane item={item} />
+              <div className="w-2/3 flex flex-col text-mainText">
+                <div className="text-2xl">{item.title}</div>
+                <div className="">{item.description}</div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </>
   );
 };
