cf8ed64d9dd623b6c0378a513fd5838e7d88d5e6
Jacob <osarukun@live.com>
2024-09-23 14:28:29 -0400

    Feat: Github page and website page buttons

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { ProjectItem } from "./ProjectList";
 import PreviewPane from "./PreviewPane";
+import { FaGithub } from 'react-icons/fa';
 
 interface Props {
   item: ProjectItem;
@@ -12,8 +13,7 @@ const ProjectCard = (props: Props) => {
 
   return (
     <div
-      className="bg-bgMain hover:bg-bgHover flex flex-row items-start w-full p-2 rounded space-x-5 cursor-pointer"
-      onClick={() => window.location.href = item.route}
+      className="bg-bgMain hover:bg-bgHover flex flex-row items-start w-full p-2 rounded space-x-5"
     >
       <div className="w-5/12">
         <PreviewPane item={item} />
@@ -26,7 +26,7 @@ const ProjectCard = (props: Props) => {
           {item.tagsList.map((tag, index) => (
             <Link
               key={index}
-              className="bg-bgHighlight p-1 flex items-center text-dimText hover:bg-bgSecondary rounded"
+              className="bg-bgHighlight p-1 flex items-center text-dimText hover:bg-bgTertiary rounded"
               href={tag.link}
               onClick={(e) => e.stopPropagation()}
             >
@@ -34,6 +34,15 @@ const ProjectCard = (props: Props) => {
               <span>{tag.title}</span>
             </Link>
           ))}
+        </div>
+        <div className="p-14" />
+        <div className="self-end flex flex-row space-x-2">
+          <Link className="flex items-center p-1 bg-accentGreen hover:bg-hoverAccentGreen rounded" href={item.git}>
+            <FaGithub className="mr-2" /><span>Github Page</span>
+          </Link>
+          <Link className="flex items-center p-1 hover:bg-bgTertiary bg-bgHighlight rounded" href="/website">
+            Visit Website Page
+          </Link>
         </div>
       </div>
     </div>
